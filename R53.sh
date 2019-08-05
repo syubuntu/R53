2b55e826f190ebc31e3556dacee13136d60e699d
Akio Ogata <akio.ogata@zaisan.net>
2019-08-05 17:17:41 +0900

    Initial Commit

---
+++ R53.sh
@@ -0,0 +1,1 @@
+aws route53 change-resource-record-sets --hosted-zone-id HOSTED-ZONE-ID --change-batch file:////file/to/path.json
---
+++ README.md
@@ -0,0 +1,4 @@
+# check Hosted Zone ID
+aws route53 list-hosted-zones
+# apply
+./R53.sh
